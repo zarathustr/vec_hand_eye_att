8f302fd86d53d67c5b5f54ffaa7124b2dc117146
Jin Wu <jin_wu_uestc@hotmail.com>
2020-03-16 13:57:57 +0800

    Update simulation.m

--- simulation.m
+++ simulation.m
@@ -2,19 +2,6 @@ clear all
 close all
 clc
 
-% load 3D_exp1_1e-4_2e-3
-
-% load data
-% 
-% for i = 1 : len
-%     if(Sun_Sensor_Available(i))
-%         ref = Sun_Sensor_J2000_Pos(i, 1 : 3);
-%         ref = ref ./ norm(ref);
-%         r = ref';
-%         break;
-%     end
-% end
-% 
 dim = 3;
 N = 1;
 M = 1;
@@ -22,29 +9,13 @@ M = 1;
 v_err = 1e-5;
 h_err = 1e-4;
 
-% r = randn(dim, N);
-% A = randn(dim, dim, M);
-% A = A * A' - eye(dim);
+r = randn(dim, N);
+A = randn(dim, dim, M);
 
 b = zeros(dim, N);
 B = zeros(dim, dim, M);
 
-% R = orthonormalize(randn(dim, dim));
-
-A = [
- -0.91489, -0.37563, -0.14788;
- -0.37563,  0.65793,  0.65271;
- -0.14788,  0.65271, -0.74304];
-
-R = [
- 0.0056824, -0.85853,  0.51272;
-   0.38732, -0.47082, -0.79266;
-   0.92193,  0.20309,  0.32985];
-R = orthonormalize(R);
-
-r = [0.93976;
-  -0.31362;
-  -0.13596];
+R = orthonormalize(randn(dim, dim));
 
 for i = 1 : N
     r(:, i) = r(:, i) ./ norm(r(:, i));
@@ -57,26 +28,6 @@ for i = 1 : M
     B(:, :, i) = R * A(:, :, i) * R' + h_err * randn(dim, dim);
     B(:, :, i) = orthonormalize(B(:, :, i));
 end
-
-% N = 1;
-% M = 1;
-% dim = 3;
-% 
-% A = [
-%  -0.91489, -0.37563, -0.14788;
-%  -0.37563,  0.65793,  0.65271;
-%  -0.14788,  0.65271, -0.74304];
-% 
-% R = [
-%  0.0056824, -0.85853,  0.51272;
-%    0.38732, -0.47082, -0.79266;
-%    0.92193,  0.20309,  0.32985];
-% R = orthonormalize(R);
-% 
-% r = [0.93976;
-%   -0.31362;
-%   -0.13596];
-
 
 P = b;
 Q = r;
@@ -91,6 +42,4 @@ x = pinv(H + kron(Q', eye(dim, dim))' * kron(Q', eye(dim, dim))) * kron(Q', eye(
 RR = reshape(x, [dim, dim]);
 
 R_err = norm(orthonormalize(RR) - R, 'inf')
-
-% save 3D_exp1_1e-4_2e-3
 
